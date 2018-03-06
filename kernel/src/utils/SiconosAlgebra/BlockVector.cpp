0879cc3c5aefe3fc46ec6c23a985cedf7c5288cb
Maurice Bremond <Maurice.Bremond@inria.fr>
2018-03-06 15:28:53 +0100

    Revert "[kernel] method to set vectors of a BlockVector"
    This reverts commit 015f782b9382fe0c8ec0fd92fbca3aa87e8767c4.

--- kernel/src/utils/SiconosAlgebra/BlockVector.cpp
+++ kernel/src/utils/SiconosAlgebra/BlockVector.cpp
@@ -314,13 +314,6 @@ void BlockVector::setVectorPtr(unsigned int pos, SP::SiconosVector v)
   updateTabIndex();
 }
 
-void BlockVector::setAllVect(VectorOfVectors& v)
-{
-  _vect = v;
-  updateSizeV();
-  updateTabIndex();
-}
-
 SP::SiconosVector BlockVector::operator [](unsigned int pos)
 {
   return  _vect[pos];
