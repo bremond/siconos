dd304aa9e85c31c2bd51fc9c48975a2fd4022c89
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-04-04 14:25:07 +0200

    test ci for pages

---
+++ CI/make_siconos_doc.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+apt-get update -qq && apt-get install -y -qq cmake git-core wget make \
+			      cmake libboost-dev libgmp-dev swig gcc gfortran g++ liblapack-dev libatlas-base-dev \
+			      lp-solve liblpsolve55-dev python3-dev libpython3-dev bash swig
+
+pip install -r ./CI/requirements.txt
+mkdir build
+cd build
+cmake ../ -DUSER_OPTIONS_FILE=../CI/siconos_docs.cmake
+make doc
