662ff93fd4f8b6f1ff0c15fb0318fb4666c327e2
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-04-04 15:00:03 +0200

    test

--- CI/make_siconos_doc.sh
+++ CI/make_siconos_doc.sh
@@ -3,9 +3,9 @@ apt-get update -qq && apt-get install -y -qq cmake git-core wget make \
 			      cmake libboost-dev libgmp-dev swig gcc gfortran g++ liblapack-dev libatlas-base-dev \
 			      lp-solve liblpsolve55-dev python3-dev libpython3-dev bash swig doxygen python3-dev python3-pip
 
-pip install -r ./docs/requirements.txt
+pip3 install -r ./docs/requirements.txt
 mkdir build
 cd build
-cmake ../ -DUSER_OPTIONS_FILE=../CI/siconos_docs.cmake
+cmake ../ -DUSER_OPTIONS_FILE=$HOME/siconos-mirror/CI/siconos_docs.cmake
 make doxygen
 make doc
