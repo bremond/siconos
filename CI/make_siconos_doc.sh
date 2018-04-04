1b2ce9b3def465319e636d52e775e19bd752782b
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-04-04 14:56:28 +0200

    test

--- CI/make_siconos_doc.sh
+++ CI/make_siconos_doc.sh
@@ -1,4 +1,8 @@
 #!bin/bash
+apt-get update -qq && apt-get install -y -qq cmake git-core wget make \
+			      cmake libboost-dev libgmp-dev swig gcc gfortran g++ liblapack-dev libatlas-base-dev \
+			      lp-solve liblpsolve55-dev python3-dev libpython3-dev bash swig doxygen python3-dev python3-pip
+
 pip install -r ./docs/requirements.txt
 mkdir build
 cd build
