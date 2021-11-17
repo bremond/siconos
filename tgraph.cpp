1dbc91e80f80845eed453d788dcab32034a7af3a
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-11-17 10:13:46 +0100

    add vecS graph (X*) bench + templated fixture

--- tgraph.cpp
+++ tgraph.cpp
@@ -12,46 +12,11 @@
 #include <boost/graph/erdos_renyi_generator.hpp>
 #include <boost/random/linear_congruential.hpp>
 
+#include "XSiconosGraph.hpp"
 #include "SiconosKernel.hpp"
 
+
 static const size_t SIZE = 100000;
-
-std::random_device dev;
-std::mt19937 rng(dev());
-std::uniform_int_distribution<std::mt19937::result_type> random_type(0,3);
-std::uniform_int_distribution<std::mt19937::result_type> random_indice(0,SIZE-1);
-
-template <typename T, size_t N>
-std::array<T, N> random_vector_stack(std::function<T(decltype(random_type(rng)))> fun) {
-  std::array<T, N> a;
-  std::generate(a.begin(), a.end(), [&] {
-    return fun(random_type(rng));
-  });
-  return a;
-}
-
-template <typename T, size_t N>
-auto random_vector_heap(std::function<T(decltype(random_type(rng)))> fun) {
-  std::shared_ptr<std::vector<T> > a(new std::vector<T>());
-  a->resize(N);
-  std::generate(a->begin(), a->end(), [&] {
-      return fun(random_type(rng));
-    });
-  return a;
-}
-
-
-template<typename T>
-auto random_pitem(const T& array)
-{
-  return array[random_indice(rng)];
-}
-
-template<typename T>
-auto random_item(const T& array)
-{
-  return &array[random_indice(rng)];
-}
 
 SP::LagrangianLinearTIDS newBall()
 {
@@ -68,45 +33,86 @@ struct FlatDS
   std::array<double, 3> position;
   std::array<double, 3> velocity;
   std::array<double, 9> mass;
-  // padding...
   bool operator < (const FlatDS& ds) const { return id < ds.id; };
+  bool operator == (const FlatDS& ds) const { return id == ds.id; };
 };
 
+template<typename G>
+typename G::vertex_t new_vertex(const G& graph, size_t i) {};
+
+template<typename G>
 struct SiconosGraphBench : public benchmark::Fixture
 {
-  typedef SiconosGraph < int, int,
-                         boost::no_property, boost::no_property,
-                         boost::no_property > IntGraph;
-  typedef SiconosGraph < std::shared_ptr<FlatDS>, int,
-                         boost::no_property, boost::no_property,
-                         boost::no_property > PFlatDSGraph;
-  typedef SiconosGraph < FlatDS, int,
-                         boost::no_property, boost::no_property,
-                         boost::no_property > FlatDSGraph;
-
-  DynamicalSystemsGraph graph;
-  PFlatDSGraph pflatds_graph;
-  std::shared_ptr<FlatDSGraph> flatds_graph = std::shared_ptr<FlatDSGraph>(new FlatDSGraph{});
-  IntGraph int_graph;
+  G graph;
 
   void SetUp(const ::benchmark::State& state)
   {
     for(unsigned int i=0; i<SIZE; ++i)
     {
-      graph.add_vertex(newBall());
-      pflatds_graph.add_vertex(std::shared_ptr<FlatDS>(new FlatDS{}));
-      flatds_graph->add_vertex(FlatDS{i});
-      int_graph.add_vertex(i);
+      graph.add_vertex(new_vertex(graph, i));
     }
-
   }
 
   void TearDown(const ::benchmark::State& state)
   {
+    graph.clear();
   }
 };
 
-BENCHMARK_F(SiconosGraphBench, DynamicalSystemAccess)(benchmark::State& state)
+
+// struct SiconosGraphBench : public benchmark::Fixture
+// {
+//   typedef SiconosGraph < size_t, size_t,
+//                          FlatDS, boost::no_property,
+//                          boost::no_property > IntGraph;
+
+//   typedef XSiconosGraph < size_t, size_t,
+//                          FlatDS, boost::no_property,
+//                          boost::no_property > XIntGraph;
+//   typedef SiconosGraph < std::shared_ptr<FlatDS>, int,
+//                          boost::no_property, boost::no_property,
+//                          boost::no_property > PFlatDSGraph;
+//   typedef SiconosGraph < FlatDS, int,
+//                          boost::no_property, boost::no_property,
+//                          boost::no_property > FlatDSGraph;
+
+//   typedef XSiconosGraph < FlatDS, int,
+//                           boost::no_property, boost::no_property,
+//                           boost::no_property > XFlatDSGraph;
+
+//   DynamicalSystemsGraph graph;
+//   PFlatDSGraph pflatds_graph;
+//   std::shared_ptr<FlatDSGraph> flatds_graph = std::shared_ptr<FlatDSGraph>(new FlatDSGraph{});
+//   std::shared_ptr<XFlatDSGraph> xflatds_graph = std::shared_ptr<XFlatDSGraph>(new XFlatDSGraph{});
+//   IntGraph int_graph;
+//   XIntGraph xint_graph;
+
+//   void SetUp(const ::benchmark::State& state)
+//   {
+//     for(unsigned int i=0; i<SIZE; ++i)
+//     {
+//       graph.add_vertex(newBall());
+//       pflatds_graph.add_vertex(std::shared_ptr<FlatDS>(new FlatDS{}));
+//       flatds_graph->add_vertex(FlatDS{i});
+//       xflatds_graph->add_vertex(FlatDS{i});
+//       int_graph.add_vertex(i);
+//       xint_graph.add_vertex(i);
+//     }
+
+//   }
+
+//   void TearDown(const ::benchmark::State& state)
+//   {
+//   }
+// };
+
+template<>
+DynamicalSystemsGraph::vertex_t new_vertex(const DynamicalSystemsGraph& g, size_t i)
+{
+  return newBall();
+}
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, DynamicalSystemAccess, DynamicalSystemsGraph)(benchmark::State& state)
 {
   DynamicalSystemsGraph::VIterator vi,vend;
 
@@ -122,15 +128,25 @@ BENCHMARK_F(SiconosGraphBench, DynamicalSystemAccess)(benchmark::State& state)
   }
 }
 
-BENCHMARK_F(SiconosGraphBench, PFlatDSAccess)(benchmark::State& state)
+typedef SiconosGraph < std::shared_ptr<FlatDS>, int,
+                       boost::no_property, boost::no_property,
+                       boost::no_property > PFlatDSGraph;
+
+template<>
+PFlatDSGraph::vertex_t new_vertex(const PFlatDSGraph& g, size_t i)
+{
+  return std::shared_ptr<FlatDS>(new FlatDS{i});
+};
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, PFlatDSAccess, PFlatDSGraph)(benchmark::State& state)
 {
   PFlatDSGraph::VIterator vi,vend;
 
   for (auto _ : state)
   {
-    for(std::tie(vi,vend) = pflatds_graph.vertices(); vi!=vend; ++vi)
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
     {
-      auto ds = pflatds_graph.bundle(*vi);
+      auto ds = graph.bundle(*vi);
       auto velocity = ds->velocity;
       benchmark::DoNotOptimize(ds);
       benchmark::DoNotOptimize(velocity);
@@ -138,15 +154,25 @@ BENCHMARK_F(SiconosGraphBench, PFlatDSAccess)(benchmark::State& state)
   }
 }
 
-BENCHMARK_F(SiconosGraphBench, FlatDSAccess)(benchmark::State& state)
+typedef SiconosGraph < FlatDS, int,
+                       boost::no_property, boost::no_property,
+                       boost::no_property > FlatDSGraph;
+
+template<>
+FlatDSGraph::vertex_t new_vertex(const FlatDSGraph& g, size_t i)
+{
+  return FlatDS{i};
+};
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, FlatDSAccess, FlatDSGraph)(benchmark::State& state)
 {
   FlatDSGraph::VIterator vi,vend;
 
   for (auto _ : state)
   {
-    for(std::tie(vi,vend) = flatds_graph->vertices(); vi!=vend; ++vi)
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
     {
-      auto ds = flatds_graph->bundle(*vi);
+      auto ds = graph.bundle(*vi);
       auto velocity = ds.velocity;
       benchmark::DoNotOptimize(ds);
       benchmark::DoNotOptimize(velocity);
@@ -154,17 +180,177 @@ BENCHMARK_F(SiconosGraphBench, FlatDSAccess)(benchmark::State& state)
   }
 }
 
-BENCHMARK_F(SiconosGraphBench, IntAccess)(benchmark::State& state)
+typedef XSiconosGraph < FlatDS, int,
+                        boost::no_property, boost::no_property,
+                        boost::no_property > XFlatDSGraph;
+
+template<>
+XFlatDSGraph::vertex_t new_vertex(const XFlatDSGraph& g, size_t i)
+{
+  return FlatDS{i};
+};
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, XFlatDSAccess, XFlatDSGraph)(benchmark::State& state)
+{
+  XFlatDSGraph::VIterator vi,vend;
+
+  for (auto _ : state)
+  {
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
+    {
+      auto ds = graph.bundle(*vi);
+      auto velocity = ds.velocity;
+      benchmark::DoNotOptimize(ds);
+      benchmark::DoNotOptimize(velocity);
+    }
+  }
+}
+
+typedef SiconosGraph < size_t, size_t,
+                          FlatDS, boost::no_property,
+                          boost::no_property > IntGraph;
+
+template<>
+IntGraph::vertex_t new_vertex(const IntGraph& g, size_t i)
+{
+  return i;
+};
+
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, IntAccess, IntGraph)(benchmark::State& state)
 {
   IntGraph::VIterator vi,vend;
 
   for (auto _ : state)
   {
-    for(std::tie(vi,vend) = int_graph.vertices(); vi!=vend; ++vi)
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
     {
-      int res = int_graph.bundle(*vi);
+      int res = graph.bundle(*vi);
       benchmark::DoNotOptimize(res);
     }
+  }
+}
+
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, IntPropertyAccess, IntGraph)(benchmark::State& state)
+{
+  IntGraph::VIterator vi,vend;
+
+  for (auto _ : state)
+  {
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
+    {
+      auto ds = graph.properties(*vi);
+      auto velocity = ds.velocity;
+      benchmark::DoNotOptimize(ds);
+      benchmark::DoNotOptimize(velocity);
+    }
+  }
+}
+
+typedef XSiconosGraph < size_t, size_t,
+                       FlatDS, boost::no_property,
+                       boost::no_property > XIntGraph;
+
+template<>
+XIntGraph::vertex_t new_vertex(const XIntGraph& g, size_t i)
+{
+  return i;
+};
+
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, XIntAccess, XIntGraph)(benchmark::State& state)
+{
+  XIntGraph::VIterator vi,vend;
+
+  for (auto _ : state)
+  {
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
+    {
+      int res = graph.bundle(*vi);
+      benchmark::DoNotOptimize(res);
+    }
+  }
+}
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, XIntPropertyAccess, XIntGraph)(benchmark::State& state)
+{
+  XIntGraph::VIterator vi,vend;
+
+  for (auto _ : state)
+  {
+    for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
+    {
+      auto ds = graph.properties(*vi);
+      auto velocity = ds.velocity;
+      benchmark::DoNotOptimize(ds);
+      benchmark::DoNotOptimize(velocity);
+    }
+  }
+}
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, IntAddRemove, IntGraph)(benchmark::State& state)
+{
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  IntGraph::VIterator vi,vend;
+
+  for (auto _ : state)
+  {
+//    for(size_t i=0; i<graph.size()/10; ++i)
+    {
+      std::uniform_int_distribution<std::mt19937::result_type> random_indice(0,graph.size()-1);
+      graph.update_vertices_indices();
+
+      size_t indx = random_indice(rng);
+      if (graph.is_vertex(indx))
+      {
+        graph.remove_vertex(indx);
+        graph.add_vertex(new_vertex(graph, indx));
+      }
+    }
+
+    // for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
+    // {
+    //   auto ds = graph.properties(*vi);
+    //   auto velocity = ds.velocity;
+    //   benchmark::DoNotOptimize(ds);
+    //   benchmark::DoNotOptimize(velocity);
+    // }
+  }
+}
+
+BENCHMARK_TEMPLATE_F(SiconosGraphBench, XIntAddRemove, XIntGraph)(benchmark::State& state)
+{
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  XIntGraph::VIterator vi,vend;
+
+  for (auto _ : state)
+  {
+
+//    for(size_t i=0; i<graph.size()/10; ++i)
+    {
+      std::uniform_int_distribution<std::mt19937::result_type> random_indice(0,graph.size()-1);
+//    xint_graph.update_vertices_indices();
+
+      size_t indx = random_indice(rng);
+      if (graph.is_vertex(indx))
+      {
+        graph.remove_vertex(indx);
+        graph.add_vertex(new_vertex(graph, indx));
+      }
+    }
+
+    // for(std::tie(vi,vend) = graph.vertices(); vi!=vend; ++vi)
+    // {
+    //   auto ds = graph.properties(*vi);
+    //   auto velocity = ds.velocity;
+    //   benchmark::DoNotOptimize(ds);
+    //   benchmark::DoNotOptimize(velocity);
+    // }
   }
 }
 
