d30ae1c16bd0239049ba9e4fbb498c59b278794e
Maurice Bremond <Maurice.Bremond@inria.fr>
2022-06-23 18:12:24 +0200

    compil & exec ok

--- SiconosData.hpp
+++ SiconosData.hpp
@@ -1,11 +1,20 @@
 #ifndef SICONOS_DATA_HPP
 #define SICONOS_DATA_HPP
 
+#include <type_traits>
 #include <variant>
 #include <tuple>
 #include <boost/pfr.hpp>
 #include <boost/hana.hpp>
 #include <boost/hana/ext/std/tuple.hpp>
+
+
+
+template<typename U, typename... T>
+constexpr bool contains(std::tuple<T...>)
+{
+  return (std::is_same_v<U, T> || ...);
+}
 
 namespace siconos
 {
@@ -13,57 +22,61 @@ namespace siconos
   template<typename OSI, typename ...Ds>
   struct data
   {
-    using env = typename OSI::Env;
-    using indice = env::indice;
-    using scalar = env::scalar;
+    using osi = OSI;
+    using env = osi::env;
+    using indice = env::indice::type;
+    using scalar = env::scalar::type;
+    using graph_t = env::graph::type;
 
     template<typename T>
-    using collection = std::array<typename env::collection<T>,
-                                  typename OSI::memory_size>;
+    using collection =
+      std::conditional<contains<T>(typename osi::keep::type{}),
+                       std::array<typename env::collection<T>::type, osi::keep::value>,
+                       std::array<typename env::collection<T>::type, 1>>::type;
 
     using types = std::variant<Ds...>;
     std::tuple<collection<typename Ds::type>...> collections;
 
-    env::indice counter = 0;
-    env::graph graph;
+    indice counter = 0;
+    graph_t graph;
 
-    collection<typename env::vdescriptor> vdescriptors;
+    collection<typename env::vdescriptor::type> vdescriptors;
 
   };
 
   template<typename OSI>
   static constexpr auto make_data()
   {
-    using env = typename OSI::env;
-    using model = OSI::model;
-    return std::apply([]<typename ...Ds>(Ds&&...) { return data<OSI,Ds...>{}; },
-                      boost::pfr::structure_to_tuple(model{}));
+    using formulation = OSI::formulation;
+    using env = OSI::env;
+
+    return std::apply([]<typename ...Ds>(Ds&&...) { return data<OSI, Ds...>{}; },
+                      typename formulation::dynamical_system<env>::attributes{});
 
   }
 
   template<typename T, typename D>
-  typename D::collection<typename T::type>::value_type&
-  get(const T, D& data)
+  typename D::collection<typename T::type>::value_type::value_type&
+  get(const typename D::env::indice::type step, D& data)
   {
     constexpr typename D::types t = T{};
-    return std::get<t.index()>(data.collections).back();
+    auto& array = std::get<t.index()>(data.collections);
+    return array[step % std::size(array)].back();
   };
-
-
 
   void for_each(auto&& fun, auto& collections)
   {
     std::apply([&fun](auto&&... args) { ((fun(args)), ...);}, collections);
   };
 
-  auto add_item(auto& data)
+  auto add_item(const auto step, auto& data)
   {
     auto vd = data.graph.add_vertex(data.counter++);
-    data.vdescriptors.push_back(vd);
+    data.vdescriptors[step%std::size(data.vdescriptors)].push_back(vd);
 
     for_each(
-      [](auto& a)
-      { a.push_back(typename std::decay_t<decltype(a)>::value_type{}); },
+      [&step](auto& a)
+      { a[step%std::size(a)].push_back(typename std::decay_t<decltype(a)>::value_type::value_type{}); },
       data.collections);
 
     return std::size(data.vdescriptors);
@@ -75,14 +88,14 @@ namespace siconos
     a.pop_back();
   }
 
-  void remove_item(const auto i, auto& data)
+  void remove_item(const auto i, const auto step, auto& data)
   {
-    auto& vd = data.vdescriptors[i];
+    auto& vd = data.vdescriptors[step%std::size(data.vdescriptors)][i];
 
     data.graph.remove_vertex(vd);
 
-    move_back(i, data.vdescriptors);
-    for_each([&i](auto &a) { move_back(i, a);}, data.collections);
+    move_back(i, data.vdescriptors[step%std::size(data.vdescriptors)]);
+    for_each([&i,&step](auto &a) { move_back(i, a[step%std::size(a)]);}, data.collections);
   }
 };
 
--- tnonos.cpp
+++ tnonos.cpp
@@ -10,61 +10,87 @@ using fmt::print;
 
 struct env
 {
-  using scalar = double;
-  using indice = size_t;
+  struct scalar
+  {
+    using type = double;
+  };
 
-  using graph = SiconosGraph < indice, indice,
-                               boost::no_property,
-                               boost::no_property,
-                               boost::no_property >;
+  struct indice
+  {
+    using type = size_t;
+  };
 
-  using vdescriptor = graph::VDescriptor;
+  struct graph
+  {
+    using type = SiconosGraph < indice::type, indice::type,
+                                boost::no_property,
+                                boost::no_property,
+                                boost::no_property >;
+  };
+
+  struct vdescriptor
+  {
+    using type = graph::type::VDescriptor;
+  };
+
   template<typename T>
-  using collection = std::vector<T>;
+  struct collection
+  {
+    using type = std::vector<T>;
+  };
 
-  template<indice N>
-  using vector = std::array<scalar, N>;
+  template<indice::type N>
+  struct vector
+  {
+    using type = std::array<scalar::type, N>;
+  };
 
-  template<indice N, indice M>
-  using matrix = std::array<scalar, N*M>;
+  template<indice::type N, indice::type M>
+  struct matrix
+  {
+    using type = std::array<scalar::type, N*M>;
+  };
 
 };
 
 struct param
 {
-  static constexpr size_t dof = 3;
+  struct dof
+  {
+    static constexpr auto value = 3;
+  };
 };
 
 namespace siconos
 {
+
   template<typename Param>
   struct lagrangian
   {
     using param = Param;
 
-    static constexpr auto dof = param::dof;
+    static constexpr auto dof = param::dof::value;
 
-    struct time_invariant_dynamical_system
+    template<typename Env>
+    struct dynamical_system
     {
+      using env = Env;
+
       struct mass_matrix
       {
-        template<typename Env>
-        using type = Env::template matrix<dof, dof>;
-      } mass_matrix;
+        using type = typename env::matrix<dof,dof>::type;
+      };
 
-      struct q
-      {
-        template<typename Env>
-        using type = Env::template vector<dof>;
-      } q;
+      struct q { using type = typename env::vector<dof>::type; };
 
-      struct velocity
-      {
-        template<typename Env>
-        using type = Env::template vector<dof>;
-      } velocity;
+      struct velocity { using type = typename env::vector<dof>::type; };
+
+      using attributes =
+        std::tuple<mass_matrix, q, velocity>;
+
     };
 
+    template<typename Env>
     struct relation
     {
     };
@@ -73,16 +99,22 @@ namespace siconos
   template<typename Env, typename Param>
   struct nonsmooth_law
   {
+    using env = Env;
+    using scalar = env::scalar;
+
     struct newton_impact_friction
     {
+      struct e : scalar{ scalar::type value; } _e;
+      struct mu : scalar{} _mu;
+
+      using attributes = std::tuple<e, mu>;
     };
+
     struct newton_impact
     {
-      struct e
-      {
-        template<typename Env>
-        using type = typename env::scalar;
-      } e;
+      struct e : scalar{};
+
+      using attributes = std::tuple<e>;
     };
   };
 
@@ -90,35 +122,58 @@ namespace siconos
   template<typename Env>
   struct one_step_integrator
   {
-    template<typename Formulation, typename Param>
+    template<typename Form, typename Param>
     struct moreau_jean
     {
-      using formulation = Formulation;
+      using env = Env;
 
-      static constexpr typename Formulation::env::indice memory_size = 2;
-    }
+
+      static constexpr auto theta = Param::theta;
+
+      using formulation = Form;
+      using system = typename formulation::dynamical_system<env>;
+
+
+      template<env::indice::type N, typename ...As>
+      struct keeper
+      {
+        static constexpr env::indice::type value = N;
+        using type = std::tuple<As...>;
+      };
+
+      using keep = keeper<2, typename system::q, typename system::velocity>;
+
+    };
   };
 
   template<typename Env>
   struct time_discretization
   {
-  }
+  };
 }
+
+template<typename Env>
+struct moreau_jean_param
+{
+  static constexpr typename Env::scalar::type theta=0.5;
+};
 
 using namespace siconos;
 
 int main()
 {
   using formulation = lagrangian<param>;
-  using osi = one_step_integrator<env>::moreau_jean<formulation>;
+  using osi = one_step_integrator<env>::moreau_jean<formulation, moreau_jean_param<env>>;
+
+  using velocity_t = formulation::dynamical_system<env>::velocity;
 
   auto data_ds = make_data<osi>();
 
   for_each([](auto& a) { print("{:d}\n", std::size(a)); }, data_ds.collections);
 
-  add_item(data_ds);
+  add_item(0, data_ds);
 
-  auto& velocity = get(ds().velocity, data_ds);
+  auto& velocity = get<velocity_t>(0, data_ds);
   velocity[0] = 1.0;
   velocity[1] = 2.0;
 
@@ -126,12 +181,12 @@ int main()
 
   for_each([](auto& a) { print("{0}\n", a); }, data_ds.collections);
 
-  add_item(data_ds);
-  add_item(data_ds);
+  add_item(0, data_ds);
+  add_item(0, data_ds);
   print("---\n");
   for_each([](auto& a) { print("{0}\n", a); }, data_ds.collections);
 
-  remove_item(1, data_ds);
+  remove_item(0, 1, data_ds);
   print("---\n");
   for_each([](auto& a) { print("{0}\n", a); }, data_ds.collections);
 }
