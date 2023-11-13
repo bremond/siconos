fb60ce46f212d49893193ef990ab313c4f9bdfe3
Maurice Bremond <Maurice.Bremond@inria.fr>
2023-11-13 19:06:47 +0100

    [soa] first python tests

--- soa/src/siconos/pybind/main.cpp
+++ soa/src/siconos/pybind/main.cpp
@@ -1,6 +1,11 @@
+#include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
 
 #include "siconos/siconos.hpp"
+
+namespace py = pybind11;
 
 namespace siconos::config::disks {
 
@@ -22,12 +27,11 @@ using params = map<iparam<"dof", 3>>;
 namespace siconos::python::disks {
 
 using namespace siconos;
-namespace py = pybind11;
 namespace some = siconos::storage::some;
 namespace config = siconos::config::disks;
 using siconos::storage::pattern::wrap;
 
-auto make_storage()
+auto imake_storage()
 {
   return storage::make<
       standard_environment<config::params>, config::simulation,
@@ -42,16 +46,45 @@ auto make_storage()
           storage::unbounded_diagonal<config::osi::mass_matrix_assembled>>>();
 }
 
-using data_t = std::decay_t<decltype(make_storage())>;
+using idata_t = std::decay_t<decltype(imake_storage())>;
 
-auto add_disk(data_t& data) { return storage::add<config::disk>(data); };
+struct data_t {
+  data_t() : _data(imake_storage()){};
+
+  idata_t& operator()() { return _data; };
+
+  idata_t _data;
+};
+
+data_t make_storage() { return data_t(); };
+
+decltype(auto) add_disk(data_t& data)
+{
+  return storage::add<config::disk>(data());
+};
+
+using disk_t = std::decay_t<decltype(storage::add<config::disk>(idata_t{}))>;
+
 }  // namespace siconos::python::disks
 
-PYBIND11_MODULE(nonos, m)
+// namespace PYBIND11_NAMESPACE {
+// namespace detail {
+// template <>
+// struct type_caster<siconos::python::disks::data_t> {};
+// }}
+PYBIND11_MODULE(_nonos, m)
 {
+  using disk_t = siconos::python::disks::disk_t;
   auto disks = m.def_submodule("disks");
+  py::class_<siconos::python::disks::data_t>(disks, "data_t");
+  py::class_<disk_t>(disks, "disk_t")
+      .def("q", &disk_t::q, py::return_value_policy::reference)
+      .def("velocity", &disk_t::velocity, py::return_value_policy::reference)
+      .def("mass_matrix", &disk_t::mass_matrix,
+           py::return_value_policy::reference);
+
   disks.doc() = R"pbdoc(
-        Nonos disks
+        Nonos m
         -----------
 
         .. currentmodule:: nonos
@@ -63,13 +96,16 @@ PYBIND11_MODULE(nonos, m)
            subtract
     )pbdoc";
 
-  disks.def("make_storage", &siconos::python::disks::make_storage, R"pbdoc(
+  disks.def("make_storage", &siconos::python::disks::make_storage,
+            py::return_value_policy::reference, R"pbdoc(
         Create a new data object for 2D disks simulation
     )pbdoc");
 
-  disks.def("add_disk", &siconos::python::disks::add_disk, R"pbdoc(
+  disks.def("add_disk", &siconos::python::disks::add_disk,
+            py::return_value_policy::reference, R"pbdoc(
         Add disk
     )pbdoc");
 
   disks.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 }
---
+++ soa/src/siconos/tests/test_nonos.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import nonos as m
+
+def test_version():
+    assert m.disks.__version__ == "dev"
+
+def test_make():
+    data = m.disks.make_storage()
+
+def test_add():
+    data = m.disks.make_storage()
+    disk = m.disks.add_disk(data)
+
+def test_q():
+    import numpy
+    data = m.disks.make_storage()
+    disk = m.disks.add_disk(data)
+    q = disk.q()
+    q = [1,2,3]
+    assert list(q) == list(disk.q())
